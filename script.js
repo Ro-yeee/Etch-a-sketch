22f2f00ade556e984cb4bc5ead5c9fbd691ad0b8
Roy Joseph <royjoseph.mec@gmail.com>
2023-06-04 17:59:57 +0530

    Erasing mode is completed, also styled the buttons

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const grid = document.getElementById('grid');
+let eraserClicked = false
+const grid = document.getElementById('grid');    
+const buttons = document.querySelectorAll('button')           //  getting the grid
+const eraserButton = buttons[0]
+const rainbowButton = buttons[1]
+const clearButton = buttons[2]
 
+
+ //  function for making divs 
 const makeDivs = (numDivs) => {
 for (let i = 0; i < numDivs; i++){
     let cells = document.createElement('div');
@@ -7,25 +14,54 @@ for (let i = 0; i < numDivs; i++){
     }
 }
 
+// function for coloring black
 const black = (target) => {
     target.classList.add('hover-effect')
 }
 
+//  function for clearing the grid
 const remove = () => {
     grid.textContent=""
     makeDivs(256)
 }
 
+//  function for toggling values
+const toggle = (booleanValue) => {
+    return booleanValue = !booleanValue;
+}
 
 makeDivs(256);
-grid.addEventListener('mousemove', (e) =>{
-    if(e.target.id === "grid") return
-    console.log(e.target.class)
-    black(e.target)
 
+const draw = () => {                                      
+    grid.addEventListener('mousemove', (e) =>{
+        if(e.target.id === "grid") return
+        if(!eraserClicked){
+            black(e.target)
+        }else{e.target.classList.remove('hover-effect')}
+    })
+}
+
+
+buttons.forEach(button => {
+   button.addEventListener('mouseenter', () =>{
+      button.classList.add('popup')
+   })
+   button.addEventListener('mouseleave', () =>{
+      button.classList.remove('popup')
+   })
 })
 
-const button = document.querySelector('button')
-button.addEventListener('click',() => {
+clearButton.addEventListener('click',() =>{
     remove()
 })
+
+eraserButton.addEventListener('click',() => {
+    eraserButton.classList.toggle('clicked')
+    eraserClicked = toggle(eraserClicked)
+})
+
+rainbowButton.addEventListener('click',() =>{
+    rainbowButton.classList.toggle('clicked')
+})
+
+draw()
