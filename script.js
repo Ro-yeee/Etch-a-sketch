18ffc33d74d8835ed5bd5f06cdb2ade9203ffadc
Roy Joseph <royjoseph.mec@gmail.com>
2023-06-05 20:51:31 +0530

    Added a slider button for selecting size of grid & made some changes in the style of the website

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let eraseMode = false
 let rainbowMode = false
 let mouseDown = false
+let rows = 16
+let cols = 16
 const grid = document.getElementById('grid');    
 const colorPicked = document.querySelector('input')
 const buttons = document.querySelectorAll('button')
+const sizePicked = document.getElementById("sizePicked")
+const sizeDisplay = document.getElementById("size-display")
 const eraserButton = buttons[0]
 const rainbowButton = buttons[1]
 const clearButton = buttons[2]
@@ -13,8 +17,11 @@ grid.onmousedown = () => (mouseDown = true)
 grid.onmouseup = () => (mouseDown = false)
 
  //  function for making divs of specified numbers inside the grid
-const makeDivs = (numDivs) => {
-for (let i = 0; i < numDivs; i++){
+const makeDivs = (rows,cols) => {
+    grid.style.setProperty('--grid-rows', rows);
+    grid.style.setProperty('--grid-cols', cols);
+    grid.textContent=""
+    for (let i = 0; i < (rows*cols); i++){
     let cells = document.createElement('div');
     grid.appendChild(cells)    
     }
@@ -23,7 +30,7 @@ for (let i = 0; i < numDivs; i++){
 //  function for clearing the grid and repopulating it with divs
 const remove = () => {
     grid.textContent=""
-    makeDivs(256)
+    makeDivs(rows,cols)
 }
 
 //  function for toggling values
@@ -59,23 +66,26 @@ rainbowButton.addEventListener('click',() =>{
 })
 clearButton.addEventListener('click',() => remove())
 
-//Main function
-const draw = () => {                                      
-    grid.addEventListener('mouseover', (e) =>{
-        if(e.target.id === "grid") return
-        if(!eraseMode && mouseDown){
-            if(rainbowMode){
-                const color = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0")
-                e.target.style.background = color
-            }else {
-                e.target.style.background = colorPicked.value
-            }
-        }else if(eraseMode && mouseDown){
-            e.target.style.background = ""
-        }
-    })
-}
+//Getting input for the size selection slider and populating the grid accordingly along with diplaying the size
+sizePicked.addEventListener('input', () =>{
+    sizeDisplay.textContent = sizePicked.value+"x"+sizePicked.value
+    rows = sizePicked.value
+    cols = sizePicked.value
+    makeDivs(rows,cols)
+})
 
-//Populating the grid and calling the main function
-makeDivs(256);
-draw()
+//Initialisation of the grid and calling the main function
+makeDivs(rows,cols)
+grid.addEventListener('mouseover', (e) =>{
+    if(e.target.id === "grid") return
+    if(!eraseMode && mouseDown){
+        if(rainbowMode){
+            const color = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0")
+            e.target.style.background = color
+        }else {
+            e.target.style.background = colorPicked.value
+        }
+    }else if(eraseMode && mouseDown){
+        e.target.style.background = ""
+    }
+})
